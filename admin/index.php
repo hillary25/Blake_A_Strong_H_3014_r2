f0a1df3077d7d3f23ab0c2d6c88ee4de1b56dacc
hillary25 <35806842+hillary25@users.noreply.github.com>
2020-02-29 20:46:45 -0500

    Initialized repository

---
+++ admin/index.php
@@ -0,0 +1,48 @@
+<?php
+    require_once '../load.php';
+
+    $time = time();
+
+    // Set the timezone to the one we're in
+    date_default_timezone_set('America/Toronto');
+
+    // Find the current hour in 24-hour format
+    $currentHour = date("H");
+
+    // If the hour is before 12pm, say "Good Morning!"
+    if($currentHour < 12) {
+        echo "Good Morning! Have a fantastic day. <br><br>";
+    // If the hour is before 6pm, say "Good Afternoon!"
+    }elseif($currentHour < 18) {
+        echo "Good Afternoon! Is it lunch time yet? <br><br>";
+    // If the hour is before or equal to 12am, say "Good Evening!"
+    }elseif($currentHour <= 24) {
+        echo"Good Evening! Time to rest up! <br><br>";
+    }
+
+    // Last successful login
+    setcookie('lastLogin', date("l jS \of F Y h:i:s A"));
+
+    // Use a cookie to store the user data of date and time in the last login field
+    if(isset($_COOKIE['lastLogin'])){
+
+        // If the cookie is traced, show the date and time
+        $showdatetime = $_COOKIE['lastLogin'];
+        echo "Your last successful login was: ".$showdatetime;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/main.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h2>Hello, welcome!</h2>
+
+</body>
+</html>
